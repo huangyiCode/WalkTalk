828f6772a3840973d07c400b44de45e17e89743b
huangyi <978140188@qq.com>
2017-05-12 11:58:07 +0800

    新建项目

---
+++ app/src/main/java/com/feicui/teach/walktalk/activity/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.feicui.teach.walktalk.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.feicui.teach.walktalk.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
