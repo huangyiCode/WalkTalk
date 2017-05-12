98d17f1f1fd991a67ca890979f928ef322bc0f4b
huangyi <978140188@qq.com>
2017-05-12 16:20:03 +0800

    主界面功能

--- app/src/main/java/com/feicui/teach/walktalk/activity/HomeActivity.java
+++ app/src/main/java/com/feicui/teach/walktalk/activity/HomeActivity.java
@@ -2,14 +2,38 @@ package com.feicui.teach.walktalk.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.feicui.teach.walktalk.BaseActivity;
 import com.feicui.teach.walktalk.R;
 
-public class HomeActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.OnClick;
+
+/**
+ * create by huangYi 主界面
+ */
+public class HomeActivity extends BaseActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+
+    @Override
+    protected void initView() {
+
+    }
+
+
+    /**
+     * 跳转
+     */
+    @OnClick(R.id.btn_home_start) void gotoNext(){
+//        openActivity();
+    }
+
 }
